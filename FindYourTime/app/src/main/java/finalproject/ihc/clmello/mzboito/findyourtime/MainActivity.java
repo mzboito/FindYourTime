2c1b96cc67d3209906cf35606593c0e202857dea
mzboito <mzboito@gmail.com>
2017-11-15 14:39:31 -0200

    first commit for our cute app :)

---
+++ FindYourTime/app/src/main/java/finalproject/ihc/clmello/mzboito/findyourtime/MainActivity.java
@@ -0,0 +1,14 @@
+package finalproject.ihc.clmello.mzboito.findyourtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*here we go*/
+    }
+}
