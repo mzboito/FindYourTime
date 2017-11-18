1cdfc15e991a81a4def71d83885c99691350cc0c
Carolina Mello <carollangemello@gmail.com>
2017-11-18 12:50:06 -0200

    get current minute and hour

--- FindYourTime/app/src/main/java/com/example/fyt/findyourtime/MainActivity.java
+++ FindYourTime/app/src/main/java/com/example/fyt/findyourtime/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.fyt.findyourtime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Calendar;
+
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getCurrentTime();
+
+    }
+
+    private void getCurrentTime()
+    {
+        // Current celphone time
+        Calendar currentTime = Calendar.getInstance();
+        int currentHour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = currentTime.get(Calendar.MINUTE);
+
+        // Test if it's getting right hour and minute
+
+        /* String currentHourStr = Integer.toString(currentHour);
+        String currentMinStr = Integer.toString(currentMinute);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(currentMinStr);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_main);
+        layout.addView(textView);*/
     }
 }
