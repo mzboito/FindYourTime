c3ada3a8fea06cba1ed445fce60beffca64ede35
Carolina Mello <carollangemello@gmail.com>
2017-12-14 18:01:51 -0200

    tela de novo Schedule adicionada

--- FindYourTime/app/src/main/java/com/example/fyt/findyourtime/NewScheduleActivity.java
+++ FindYourTime/app/src/main/java/com/example/fyt/findyourtime/NewScheduleActivity.java
@@ -2,6 +2,7 @@ package com.example.fyt.findyourtime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewScheduleActivity extends AppCompatActivity {
     Info info;
@@ -11,5 +12,13 @@ public class NewScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule);
         info = MainActivity.info;
+    }
+
+    public void registerSchedule(View view){
+
+    }
+
+    public void cancelSchedule(View view){
+        finish();
     }
 }
--- FindYourTime/app/src/main/java/com/example/fyt/findyourtime/ScheduleActivity.java
+++ FindYourTime/app/src/main/java/com/example/fyt/findyourtime/ScheduleActivity.java
@@ -1,7 +1,9 @@
 package com.example.fyt.findyourtime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         setTitle("Schedule");
+    }
+
+    public void sendNewScheduleActivity(View view) {
+        Intent intent = new Intent(this, NewScheduleActivity.class);
+        startActivity(intent);
     }
 }
